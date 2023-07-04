566157528f02a0e0dd88ff699e128a67aeea158c
Flu3nt <gnedyalkov94@gmail.com>
2023-07-04 16:18:38 +0300

    feature: solve problem three

--- 03.Gighub-Commits/app.js
+++ 03.Gighub-Commits/app.js
@@ -1,4 +1,28 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+  const usernameValue = document.getElementById("username").value;
+  const repoValue = document.getElementById("repo").value;
+  const commitsUl = document.getElementById("commits");
+
+  fetch(`https://api.github.com/repos/${usernameValue}/${repoValue}/commits`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status} (Not Found)`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      commitsUl.textContent = "";
+      for (let entry of data) {
+        const { author, message } = entry.commit;
+        const liEl = document.createElement("li");
+        liEl.textContent = `${author.name}: ${message}`;
+        commitsUl.appendChild(liEl);
+      }
+    })
+    .catch((err) => {
+      commitsUl.textContent = "";
+      const liEl = document.createElement("li");
+      liEl.textContent = err.message;
+      commitsUl.appendChild(liEl);
+    });
 }
