f2d2064a3d81e19c3ab235c04ce1b1285cfb7915
Flu3nt <gnedyalkov94@gmail.com>
2023-07-05 09:06:54 +0300

    refactor: write a new function to create li and iterate over data with forEach

--- 02.Github-Repos/scripts/app.js
+++ 02.Github-Repos/scripts/app.js
@@ -4,20 +4,25 @@ function loadRepos() {
   const firstLiEl = document.querySelector("ul li");
   reposUl.removeChild(firstLiEl);
 
-  fetch(`https://api.github.com/usera/${username}/repos`)
+  fetch(`https://api.github.com/users/${username}/repos`)
     .then((response) => response.json())
     .then((data) => {
-      for (let repo of data) {
+      data.forEach((repo) => {
         const { full_name, html_url } = repo;
-        let liEl = document.createElement("li");
-        let anchorEl = document.createElement("a");
-        anchorEl.setAttribute("href", html_url);
-        anchorEl.textContent = full_name;
-        liEl.appendChild(anchorEl);
+        const liEl = createLi(html_url, full_name);
         reposUl.appendChild(liEl);
-      }
+      });
     })
     .catch((err) => {
       console.error(err);
     });
+}
+
+function createLi(url, fullname) {
+  const liEl = document.createElement("li");
+  const anchorEl = document.createElement("a");
+  anchorEl.href = url;
+  anchorEl.textContent = fullname;
+  liEl.appendChild(anchorEl);
+  return liEl;
 }
