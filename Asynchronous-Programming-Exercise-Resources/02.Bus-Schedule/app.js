143431a9a7be2dab97b48b0acb695ce0528b7a6f
Flu3nt <gnedyalkov94@gmail.com>
2023-07-06 12:28:14 +0300

    refactor: add another solution using async await for problem two

--- Asynchronous-Programming-Exercise-Resources/02.Bus-Schedule/app.js
+++ Asynchronous-Programming-Exercise-Resources/02.Bus-Schedule/app.js
@@ -4,7 +4,64 @@ function solve() {
   const arriveBtn = document.getElementById("arrive");
   const apiURL = `http://localhost:3030/jsonstore/bus/schedule/`;
   let stopIds = [];
-  let stopIndex = 14;
+  let stopIndex = 0;
+  let currentStop;
+
+  async function getStopIds() {
+    const response = await fetch(apiURL);
+    const data = await response.json();
+
+    for (let stopId in data) {
+      stopIds.push(stopId);
+    }
+  }
+
+  getStopIds();
+
+  function depart() {
+    async function getCurrentStop() {
+      try {
+        const response = await fetch(`${apiURL}${stopIds[stopIndex]}`);
+        const stop = await response.json();
+
+        currentStop = stop;
+        infoBox.textContent = `Next stop ${stop.name}`;
+        departBtn.disabled = true;
+        arriveBtn.disabled = false;
+      } catch (error) {
+        infoBox.textContent = "Error";
+        departBtn.disabled = true;
+        arriveBtn.disabled = true;
+      }
+    }
+
+    getCurrentStop();
+  }
+
+  function arrive() {
+    infoBox.textContent = `Arriving at ${currentStop.name}`;
+    arriveBtn.disabled = true;
+    departBtn.disabled = false;
+    if (currentStop.name === "Depot") {
+      departBtn.disabled = true;
+    }
+    stopIndex++;
+  }
+
+  return {
+    depart,
+    arrive,
+  };
+}
+let result = solve();
+
+function solveUsingFetch() {
+  const infoBox = document.getElementById("info");
+  const departBtn = document.getElementById("depart");
+  const arriveBtn = document.getElementById("arrive");
+  const apiURL = `http://localhost:3030/jsonstore/bus/schedule/`;
+  let stopIds = [];
+  let stopIndex = 0;
   let currentStop;
 
   fetch(apiURL)
@@ -47,4 +104,3 @@ function solve() {
     arrive,
   };
 }
-let result = solve();
